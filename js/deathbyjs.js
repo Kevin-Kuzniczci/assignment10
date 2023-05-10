9b9b49d461e4b6595861f7b1c92314743b07cdab
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-05-10 12:04:19 -0700

    Just need to finish last 3 steps!

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,6 +1,47 @@
 //STEP 1
 
+// let initialString = "webmaster";
+
+// function sortletters(str) {
+//     let letters = str.split("");
+
+//     letters.sort();
+
+//     let sortedString = letters.join("");
+
+//     return sortedString;
+// }
+
+// let newString = sortletters(initialString);
+
+// console.log("The original string, " + initialString + ", becomes " + newString);
+
+
+
 //STEP 2
+
+// let initialString = "The quick brown fox jumps over the lazy dog";
+
+// function capletters(str) {
+//     let words = str.split(" ");
+
+//     for (i = 0; i < words.length; i++) {
+//         let firstletter = words[i].charAt(0).toUpperCase();
+//         let restOfTheWord = words[i].slice(1);
+//         words[i] = firstletter + restOfTheWord;
+//     }
+
+//     let cappedString = words.join(" ");
+
+//     return cappedString;
+// }
+
+// let newString = capletters(initialString);
+
+// console.log("Before: " + initialString);
+// console.log("After: " + newString);
+
+
 
 //STEP 3
 
