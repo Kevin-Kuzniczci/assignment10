8bc2d02bc8684fedf8d4fdb50c078a433515f2ce
Kevin-Kuzniczci <60890825+Kevin-Kuzniczci@users.noreply.github.com>
2023-05-10 11:02:42 -0700

    Finished strings, starting math & date segments

--- js/strings.js
+++ js/strings.js
@@ -1,19 +1,87 @@
 //STEP 1
 
+// let username = prompt("What is your name?");
+
+// let namelength = username.length;
+
+// alert("The length of your name is: " + namelength);
+
 //STEP 2
+
+// let numericValue = prompt("Please enter a numeric value");
+
+// let index = parseInt(numericValue);
+
+// let letter = username.charAt(index);
+
+// alert("The letter at your number " + index + " in your name is " + letter);
 
 //STEP 3
 
+// let username2 = prompt("What is your last name?");
+
+// let fullname = username.concat(username2);
+
+// alert("Your name is: " + fullname);
+
+
+
 //STEP 4
+
+// let text = "The quick brown fox jumps over the lazy dog";
+
+// let index = text.indexOf("fox");
+
+// alert("The index of the word 'fox' is " + index);
 
 //STEP 5
 
+// let lastIndex = text.lastIndexOf("fox");
+
+// alert("The last index of the word 'fox' is " + lastIndex);
+
 //STEP 6
+
+// let fullname = prompt("What is your full name?");
+
+// let replacementText = text.replace("the lazy dog", fullname);
+
+// alert("Text edited! Here's the result: " + replacementText);
 
 //STEP 7
 
+// let randomWord = prompt("Please enter any word you would like, it is for a search.");
+
+// let searchEnd = text.includes(randomWord);
+
+// alert("The word " + randomWord + " is" + (searchEnd ? "" : " not") + " found in the text.");
+
+
+
 //STEP 8
+
+// let old_string = "The quick brown fox jumps over the lazy dog";
+
+// let new_string = old_string.substring(30);
+
+// let final_result = new_string.toUpperCase();
+
+// alert("The result: " + final_result);
 
 //STEP 9
 
+// let text = " THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG ";
+
+// let shortText = text.trim();
+
+// let lowercaseText = shortText.toLowerCase();
+
+// alert("Here it is in lowercase: " + lowercaseText);
+
 //STEP 10
+
+// let text = "the quick brown fox jumps over the lazy dog";
+
+// let capFirstWord = text.charAt(0).toUpperCase() + text.slice(1);
+
+// alert("Capitalized result is as follows: " + capFirstWord);
